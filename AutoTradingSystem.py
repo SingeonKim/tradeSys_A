f7a4f425c4e9a5d31f3ba8a39b26122054b1018a
geony_kkk <blueds05@gmail.com>
2024-07-12 14:51:30 +0900

    [feature] buy_nice_timing 기능 추가 및 키위 TC 추가

---
+++ AutoTradingSystem.py
@@ -0,0 +1,18 @@
+from StokerBrockerDriver import StokerBrockerDriver
+
+
+class AutoTradingSystem:
+    def __init__(self, broker):
+        self.__broker = broker
+
+
+    def buy_nice_timing(self, stock_name, input_balance):
+        prev = 0
+        for i in range(3):
+            current_price = self.__broker.get_price(stock_name)
+            if current_price <= prev:
+                return False
+            prev = current_price
+
+        self.__broker.buy(stock_name, current_price, input_balance // current_price)
+        return True
