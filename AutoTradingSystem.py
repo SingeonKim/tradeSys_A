c35aa7b53b093210ea4e94d5dd8a7b6e0661abef
donghee.lim <ldh5730@naver.com>
2024-07-12 14:48:14 +0900

    [feature] sell_nice_timing 구현

---
+++ AutoTradingSystem.py
@@ -0,0 +1,19 @@
+from StokerBrockerDriver import StokerBrockerDriver
+import time
+
+
+class AutoTradingSystem:
+    def __init__(self, broker: StokerBrockerDriver):
+        self.__broker = broker
+
+    def sell_nice_timing(self, code, count) -> bool:
+        res = []
+        for _ in range(3):
+            res.append(self.__broker.get_price(code))
+            # time.sleep(0.1)
+
+        is_downtrend = (res[0] > res[1]) and (res[1] > res[2])
+        if is_downtrend:
+            self.__broker.buy(code, res[2], count)
+
+        return is_downtrend
